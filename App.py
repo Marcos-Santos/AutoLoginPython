c639a1e3dec04f09e955c59f9e22c5f8e38cf9bf
Marcos-Santos <marcos_alexandre@msn.com>
2024-08-28 03:54:04 +0000

    Added correction for the Reader function and initialized the  web browser's code

--- App.py
+++ App.py
@@ -2,16 +2,23 @@
 # Autor: Marcos Alexandre
 
 import csv
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
-print("Inicio programa")
-
+# get the user's data from CSV file
 with open ('Dados/Dados de Acesso.csv', 'r') as arq: 
-        leitor = csv.reader(arq)
-        for linha in leitor:
-            print(linha)
+        reader = csv.reader(arq)
+        for line in reader:
+                if ("Login:" in line):
+                        user = line[1]
+                if ("Password:" in line):
+                       password = line[1]
+           
+# Open the Default Browser
+print("iniciando parte dois")
+#Toda vez ele vai verificar que versao esta sendo usado do seu Chrome e instalando o driver necessario 
+#serv = Service(ChromeDriverManager().install())
+#nav = webdriver.Chrome(service=serv)
 
-print("Teste de uso dos Dados")
-print(linha[0])
-print(linha[1])
-
-
+navegador = webdriver.Chrome().get("www.google.com")
